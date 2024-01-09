841e3a921509acbecaaf63df137eaafa0978957e
erenmestan <ermestan@gmail.com>
2024-01-09 12:00:20 +0300

    Created Tasks and tasks added to array

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,5 +1,13 @@
-function TaskList() {
-    return <div>TaskList</div>;
+import TaskShow from "./TaskShow";
+
+function TaskList({tasks}) {
+    return (
+        <div>
+            {tasks.map((task,index) => {
+                return <TaskShow key={index} task={task} />;
+            })}
+        </div>
+    );
 }
 
 export default TaskList;
